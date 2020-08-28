b37862d28e4f901cc32f177045b05e15489083e0
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-08-28 14:57:49 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,31 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.css"
+//import "./layout.css"
 import Navbar from "./globals/navbar/Navbar"
+import { createGlobalStyle } from "styled-components"
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
+      <GlobalStyle />
       <Navbar />
       {children}
     </React.Fragment>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+  color:#262626;
+  background:#fff;
+}
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
